6e770e85afcb1094f9c01d6a30aa79e75802185e
MNovakovic <marko.novakovic@mail.ru>
2018-09-20 20:54:32 +0200

    Starter Plugin Structure

--- hepek-plugin.php
+++ hepek-plugin.php
@@ -18,32 +18,36 @@
 
 if( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
+// require once the Composer Autoload
 if( file_exists( dirname( __FILE__ ). '/vendor/autoload.php')){
     require_once dirname( __FILE__ ). '/vendor/autoload.php';
 }
 
+// Difane CONSTANTS
 define('PLUGIN_PATH', plugin_dir_path(__FILE__) );
 define('PLUGIN_URL',  plugin_dir_url( __FILE__ ) );
 define('PLUGIN',  plugin_basename( __FILE__ ) );  // display name of plugin
 
 use Inc\Base\Activate;
 use Inc\Base\Deactivate;
-
+/** 
+* The code that runs during plugin activation
+*/
 function activate_hepek_plugin(){
     Activate::activate();
 }
-
+/***
+* The code that run during plugin deactivation
+*/
 function deactivate_hepek_plugin(){
     Deactivate::deactivate();
 }
 
+register_activation_hook(__FILE__, 'activate_hepek_plugin');
 
-    register_activation_hook(__FILE__, 'activate_hepek_plugin');
-
-  
-   //  require_once plugin_dir_path(__FILE__).'inc/hepek-plugin-deactivate.php';
-    register_deactivation_hook(__FILE__, 'deactivate_hepek_plugin');
-
+//  require_once plugin_dir_path(__FILE__).'inc/hepek-plugin-deactivate.php';
+register_deactivation_hook(__FILE__, 'deactivate_hepek_plugin');
+   
 if ( class_exists('Inc\\Init')) {
      Inc\Init::register_services();
 }
